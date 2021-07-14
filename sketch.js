f95050553bb6adcbd6afc05771b6808517be229e
Godbless1994 <87233515+Godbless1994@users.noreply.github.com>
2021-07-14 16:57:09 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,40 +1,30 @@
-var box;
+var boat_moving, boat, edges;
+var seaImage;
+var sea;
+function preload(){
+boat_moving = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
+seaImage = loadImage("sea.png");
 
-function setup() {
-  createCanvas(400,400);
-  background(51);
-  box = createSprite(200,200,30,30);
 
 }
 
-function draw() 
-{
+function setup(){
+  createCanvas(800,800);
+  //create boat
+  sea = createSprite(200,200,20,20);
+  sea.addImage("sea",seaImage);
   
-
-  if (keyIsDown(RIGHT_ARROW)) 
-  {
-    background("red");
-  }
-
-    if (keyIsDown(LEFT_ARROW)) 
-  {
-    background("blue");
-    
-  }
- 
-    if (keyIsDown(UP_ARROW)) 
-  {
-    background("yellow");
-   
-  }
-
-  if (keyIsDown(DOWN_ARROW)) 
-  {
-    background("green");
-  }
-
-  drawSprites();
+  boat = createSprite(400,400);
+   boat.addAnimation("moving", boat_moving); 
+  
+  edges = createEdgeSprites();
+  
 }
 
-
-
+function draw() {
+  background("blue");
+   boat.scale = 0.3;
+  sea.velocityX = -2;
+  drawSprites();
+ 
+}
